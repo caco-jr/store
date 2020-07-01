07cc44de1b38ff0020cb59b1815307c4349663c3
Carlos Costa <jr.carlosad@gmail.com>
2020-06-30 21:45:45 -0300

    feat: Iniciando projeto e montando a estrutura

---
+++ src/interfaces/index.ts
@@ -0,0 +1,17 @@
+export type Store = {
+  products: Cart[];
+};
+
+type Cart = {
+  id: number;
+  sku: number;
+  title: string;
+  description: string;
+  availableSizes: string[];
+  style: string;
+  price: number;
+  installments: number;
+  currencyId: string;
+  currencyFormat: string;
+  isFreeShipping: boolean;
+};
---
+++ src/pages/api/store/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { storeData } from './data';
+
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    res.status(200).json(storeData);
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
+};
+
+export default handler;
