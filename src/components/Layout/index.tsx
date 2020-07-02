d891917b1c36f93fb12bf57789dcf21cd853a40f
Carlos Costa <jr.carlosad@gmail.com>
2020-07-01 21:53:37 -0300

    style: Inserindo font-family

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -17,6 +17,10 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
       <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
         rel="stylesheet"
         href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
       />
