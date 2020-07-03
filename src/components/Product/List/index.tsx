864bce5750e56cdc165fd4d2b148fa5d95394989
Carlos Costa <jr.carlosad@gmail.com>
2020-07-02 23:46:55 -0300

    feat: Aplicando CSS Grid

--- src/components/Product/List/index.tsx
+++ src/components/Product/List/index.tsx
@@ -6,7 +6,11 @@ type Props = {};
 const ProductList: FunctionComponent<Props> = ({ children }) => {
   const componentClassName = 'c-product-list';
 
-  return <section className={componentClassName}>{children}</section>;
+  return (
+    <section className="container">
+      <section className={componentClassName}>{children}</section>
+    </section>
+  );
 };
 
 export default ProductList;
