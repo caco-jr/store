cc0abd0cb3f43160195641a5377e546f1568e3a3
Carlos Costa <jr.carlosad@gmail.com>
2020-07-05 19:28:33 -0300

    style: Estilizando página de produto

--- src/pages/p/[id].tsx
+++ src/pages/p/[id].tsx
@@ -1,11 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { FunctionComponent } from 'react';
 
+import './index.scss';
 import Layout from '@components/Layout';
 import { STORE_NAME } from '@utils/strings';
 import { Product } from '@interfaces/store';
 import { getProductAPI, getProductsAPI } from '@services/api';
-import { isArray } from 'util';
 import CartAdd from '@components/Cart/Add';
 import { getPrettyPriceFormat } from '@utils/price/index';
 
@@ -25,22 +25,33 @@ const ProductPage: FunctionComponent<Props> = ({ errors, response }) => {
     );
   }
 
-  const { title, description, price, currencyId } = response;
+  const { title, description, price, currencyId, featuredMedia } = response;
 
   return (
     <Layout title={`${title}`}>
       <section className={`container`}>
         <h1 className={`${componentClassName}__title`}>{title}</h1>
 
-        <span className={`${componentClassName}__description`}>
-          {description}
-        </span>
+        <section className={`${componentClassName}__container`}>
+          <figure className={`${componentClassName}__figure`}>
+            <img
+              className={`${componentClassName}__image`}
+              src={featuredMedia.baseURI}
+            />
+          </figure>
 
-        <span className={`${componentClassName}__price`}>
-          {getPrettyPriceFormat(price, currencyId)}
-        </span>
+          <section className={`${componentClassName}__content`}>
+            <span className={`${componentClassName}__description`}>
+              {description}
+            </span>
 
-        <CartAdd product={response} />
+            <span className={`${componentClassName}__price`}>
+              {getPrettyPriceFormat(price, currencyId)}
+            </span>
+
+            <CartAdd product={response} />
+          </section>
+        </section>
       </section>
     </Layout>
   );
@@ -67,7 +78,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
     const id = params?.id;
-    const response = await getProductAPI(isArray(id) ? id.join(',') : id);
+    const response = await getProductAPI(Array.isArray(id) ? id.join(',') : id);
 
     return { props: { response } };
   } catch (err) {
