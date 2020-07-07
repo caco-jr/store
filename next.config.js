279f7cff833421fd8b42fc7eb820ba6374281e75
Carlos Costa <jr.carlosad@gmail.com>
2020-07-06 23:05:38 -0300

    feat: Inserindo configuração para PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,43 @@
 const path = require('path');
+const withOffline = require('next-offline');
 
-module.exports = {
+const nextOfflineConfig = {
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ];
+  },
+};
+
+const sassConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
+
+const nextConfig = {
+  ...sassConfig,
+  ...nextOfflineConfig,
+};
+
+module.exports = withOffline(nextConfig);
