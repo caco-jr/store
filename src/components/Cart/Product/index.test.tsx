c6fe4e4503a06f91257746ca2d0416dcb94c0502
Carlos Costa <jr.carlosad@gmail.com>
2020-08-01 18:12:48 -0300

    refactor: Retirando import repetido

--- src/components/Cart/Product/index.test.tsx
+++ src/components/Cart/Product/index.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { initialStoreMock, mockStore } from 'src/__mocks__/redux/store';
 
--- src/components/Product/Card/index.test.tsx
+++ src/components/Product/Card/index.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 import ProductCard from './index';
 import { mockStoreData } from 'src/__mocks__/api/data';
