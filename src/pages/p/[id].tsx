189d07ec284863e885df22f0d2bdccaca501b84e
Carlos Costa <jr.carlosad@gmail.com>
2020-10-24 16:08:36 -0300

    refactor: Tratando GetStaticPaths e GetStaticProps

--- src/pages/p/[id].tsx
+++ src/pages/p/[id].tsx
@@ -1,1 +1,43 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+
 export { default } from '@views/p/[id]';
+import { getProductAPI, getProductsAPI } from '@services/api';
+import { slugify } from '@utils/strings';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  try {
+    const response = await getProductsAPI();
+
+    // Get the paths we want to pre-render based on products
+    const paths = response.products.map(user => ({
+      params: {
+        id: `${slugify(user.title)}---${user.id}`,
+      },
+    }));
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return {
+      paths,
+      fallback: false,
+    };
+  } catch (err) {
+    console.error(err);
+    return { paths: [], fallback: false };
+  }
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const paramID = params?.id;
+    const id = Array.isArray(paramID)
+      ? paramID.join(',')
+      : paramID?.split('---')[1];
+
+    const response = await getProductAPI(id);
+
+    return { props: { response } };
+  } catch (err) {
+    return { props: { errors: err.message } };
+  }
+};
--- src/views/p/[id].tsx
+++ src/views/p/[id].tsx
@@ -1,11 +1,9 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
 import { FunctionComponent } from 'react';
 
 import styles from './index.module.scss';
 import Layout from '@components/Layout';
-import { STORE_NAME, slugify } from '@utils/strings';
+import { STORE_NAME } from '@utils/strings';
 import { Product } from '@interfaces/store';
-import { getProductAPI, getProductsAPI } from '@services/api';
 import CartAdd from '@components/Cart/Add';
 import { getPrettyPriceFormat } from '@utils/price/index';
 import { buildImageURI } from '@utils/URIs/image/index';
@@ -54,44 +52,6 @@ const ProductPage: FunctionComponent<Props> = ({ errors, response }) => {
       </section>
     </Layout>
   );
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  try {
-    const response = await getProductsAPI();
-
-    // Get the paths we want to pre-render based on products
-    const paths = response.products.map(user => ({
-      params: {
-        id: `${slugify(user.title)}---${user.id}`,
-      },
-    }));
-
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-    return {
-      paths,
-      fallback: false,
-    };
-  } catch (err) {
-    console.error(err);
-    return { paths: [], fallback: false };
-  }
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  try {
-    const paramID = params?.id;
-    const id = Array.isArray(paramID)
-      ? paramID.join(',')
-      : paramID?.split('---')[1];
-
-    const response = await getProductAPI(id);
-
-    return { props: { response } };
-  } catch (err) {
-    return { props: { errors: err.message } };
-  }
 };
 
 export default ProductPage;
